d107b2719c3e01bdd474e2cb9ff618c320bd05fa
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2013-02-06 12:03:36 +0000

    deleting liexec directory

---
+++ src/em_mad/drm4g/drm4g/communicators/local.py
@@ -0,0 +1,56 @@
+import subprocess
+import os
+import shutil
+import re
+import drm4g.communicators 
+from drm4g.utils.url import urlparse
+
+__version__ = '0.1'
+__author__  = 'Carlos Blanco'
+__revision__ = "$Id$"
+
+class Communicator(drm4g.communicators.Communicator):
+    """
+    Interact with local resources using shell commands 
+    """
+        
+    def execCommand(self, command):
+        command_proc = subprocess.Popen(command,
+            shell = True,
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE,
+            env = os.environ)
+        return command_proc.communicate()
+        
+    def mkDirectory(self, url):
+        to_dir = self._setDir(urlparse(url).path)    
+        os.mkdir(to_dir)
+        
+    def copy(self, source_url, destination_url, execution_mode):
+        if 'file://' in source_url:
+            from_dir = urlparse(source_url).path
+            to_dir   = self._setDir(urlparse(destination_url).path)
+        else:
+            from_dir = self._setDir(urlparse(source_url).path)
+            to_dir   = urlparse(destination_url).path
+        shutil.copy(from_dir,to_dir)
+        if execution_mode == 'X':
+            os.chmod(to_dir, 0755)#execution permissions
+            
+    def rmDirectory(self, url):
+        to_dir = self._setDir(urlparse(url).path)    
+        out, err = self.execCommand("rm -rf %s" % (to_dir))
+        if err:
+            raise drm4g.communicators.ComException("Couldn't remove: %s" %(to_dir))
+    
+    def checkOutLock(self, url):   
+        to_dir = self._setDir(urlparse(url).path)
+        return os.path.isfile( '%s/.lock'% (to_dir))
+
+    #internal
+    
+    def _setDir(self, path):
+        if self.workDirectory != '~':
+            return re.compile(r'^~').sub(self.workDirectory, path)
+        else: 
+            return os.path.expanduser(path)
---
+++ src/em_mad/drm4g/drm4g/managers/loadleveler.py
@@ -0,0 +1,142 @@
+import drm4g.managers 
+from string import Template
+from drm4g.managers import sec_to_H_M_S
+import re
+
+__version__ = '0.1'
+__author__  = 'Carlos Blanco'
+__revision__ = "$Id$"
+
+# The programs needed by these utilities. If they are not in a location
+# accessible by PATH, specify their location here.
+LLCLASS  = 'llclass'    #show class information
+LLSUBMIT = 'llsubmit'   #submit ajob
+LLQ      = 'llq'        #show jobs' status
+LLCANCEL = 'llcancel'   #delete ajob
+
+class Resource (drm4g.managers.Resource):
+
+    def hostProperties(self):
+        return ('NULL', 'NULL', 'NULL', 'NULL')
+
+    def cpuProperties(self):
+        return ('NULL', '0')
+
+    def memProperties(self):
+        return ('0', '0')
+    
+    def diskProperties(self):
+        return ('0', '0')
+
+    def lrmsProperties(self):
+        return ('Loadleveler', 'Loadleveler') 
+ 
+    def dynamicNodes(self):
+        out, err = self.Communicator.execCommand("%s -l | egrep 'Maximum_slots' | awk '{print $2}'" % (LLCLASS))
+        if err:
+            raise drm4g.managers.ResourceException(' '.join(err.split('\n')))
+        total_cpu  = sum([int(elem) for elem in out.split()])
+        out, err = self.Communicator.execCommand("%s -l | egrep 'Free_slots' | awk '{print $2}'" % (LLCLASS))
+        if err:
+            raise drm4g.managers.ResourceException(' '.join(err.split('\n')))
+        free_cpu  = sum([int(elem) for elem in out.split()])
+        return (str(total_cpu), str(free_cpu))
+
+    def queuesProperties(self, searchQueue, project):
+        queue  = drm4g.managers.Queue()
+        queue.DispatchType = 'batch'
+        if searchQueue:
+            out, err = self.Communicator.execCommand("%s -l %s | -e Free_slots -e Maximum_slots | awk '{print $2}'" % (LLCLASS, searchQueue))
+            if err:
+                raise drm4g.managers.ResourceException(' '.join(err.split('\n')))
+            if self.TotalCpu != "0":
+                queue.FreeNodes, queue.Nodes = out.split()  
+            else:
+                queue.Nodes        = self.TotalCpu
+                queue.FreeNodes    = self.FreeCpu
+        else:
+            queue.Name         = 'default'
+            queue.Nodes        = self.TotalCpu
+            queue.FreeNodes    = self.FreeCpu
+        return [queue]
+
+class Job (drm4g.managers.Job):
+   
+    #loadleveler job status <--> GridWay job status
+    states_loadleveler = {'CA': 'DONE',
+                  'CK': 'ACTIVE',
+                  'C' : 'PENDING',
+                  'CP': 'ACTIVE',
+                  'D' : 'PENDING',
+                  'I' : 'PENDING',
+                  'NQ': 'SUSPENDED',
+                  'NR': 'SUSPENDED',
+                  'P' : 'ACTIVE',
+                  'E' : 'ACTIVE',
+                  'EP': 'ACTIVE',
+                  'X' : 'DONE',
+                  'XP': 'SUSPENDED',
+                  'RM': 'DONE',
+                  'RP': 'SUSPENDED',
+                  'MP': 'ACTIVE',
+                  'R' : 'ACTIVE', 
+                  'ST': 'ACTIVE',
+                  'S' : 'PENDING',
+                  'TX': 'PENDING', 
+                  'HS': 'PENDING',  
+                  'H' : 'PENDING',
+                  'V' : 'DONE',
+                  'VP': 'PENDING',
+                }
+    re_submit=re.compile(r"The job \"(\S+)\" has been submitted")
+
+    def jobSubmit(self, path_script):
+        out, err = self.Communicator.execCommand('%s %s' % (LLSUBMIT, path_script))
+        job_id = self.re_submit.search(out).group(1)
+        return job_id
+
+    def jobStatus(self):
+        command = LLQ + ' -f %st ' + self.JobId
+        out, err = self.Communicator.execCommand(command)
+        if "There is currently no job status to report" in out :
+            return 'DONE'
+        else:
+            status = out.split('\n')[2].strip()
+            return self.states_loadleveler.setdefault(status, 'UNKNOWN')
+    
+    def jobCancel(self):
+        out, err = self.Communicator.execCommand('%s %s' % (LLCANCEL, self.JobId))
+        if err: 
+            raise drm4g.managers.JobException(' '.join(err.split('\n')))
+
+    def jobTemplate(self, parameters):
+        args  = '#!/bin/bash\n'
+        args += '#@ job_name = JID_%s\n' % (parameters['environment']['GW_JOB_ID'])
+        if parameters['queue'] != 'default':
+            args += '#@ class    = $queue\n'
+        args += '#@ output   = $stdout\n'
+        args += '#@ error    = $stderr\n'
+        if int(parameters['count']) > 1 :
+            args += '#@ job_type  = parallel\n'
+        else:
+            args += '#@ job_type  = serial\n'
+        args += '#@ node = $count\n'
+        if parameters.has_key('maxWallTime'): 
+            args += '#@ wall_clock_limit = %s\n' % (sec_to_H_M_S(parameters['maxWallTime']))
+        if parameters.has_key('maxCpuTime'):
+            args += '#@ job_cpu_limit = %s\n' % (sec_to_H_M_S(parameters['maxCpuTime']))
+        if parameters.has_key('maxMemory'):
+            args += '#@ resources = ConsumableMemory(%s)\n' % (parameters['maxMemory'])
+        if parameters.has_key('tasksPerNode'):
+            args += '#@ tasks_per_node = $tasksPerNode\n' % (parameters['tasksPerNode'])
+        if parameters.has_key('PROJECT'):
+            args += '#@ account_no = $PROJECT\n'
+        args += '#@ queue\n'
+        args += ''.join(['export %s=%s\n' % (k, v) for k, v in parameters['environment'].items()])
+        args += 'cd $directory\n'
+        if parameters['jobType'] == "mpi":
+            args += 'mpiexec -np $count $executable\n'
+        else:
+            args += '$executable\n'
+        return Template(args).safe_substitute(parameters)
+
