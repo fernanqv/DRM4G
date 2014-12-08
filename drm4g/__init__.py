29420a36489ee78543f2fb4b95f2ae3436c28d63
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2014-12-08 15:17:53 +0000

    adding acct directory for accounting jobs

--- drm4g/__init__.py
+++ drm4g/__init__.py
@@ -27,7 +27,7 @@ logging.basicConfig( format='%(message)s', level = logging.INFO , stream = sys.s
 logger = logging.getLogger(__name__) 
 if exists( DRM4G_DIR ) is False  :
     logger.info( "Creating a DRM4G local configuration in '%s'" %  DRM4G_DIR )
-    abs_dir = join ( DRM4G_DIR , 'var' )
+    abs_dir = join ( DRM4G_DIR , 'var' , 'acct' )
     logger.info( "Creating '%s' directory" % abs_dir )
     os.makedirs( abs_dir )
     from  shutil import copytree
