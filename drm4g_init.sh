43daacc12fbe6abbb8680f0f8773d5713078116c
carlos <carlos@3232fcf4-bb0b-194b-8ba9-96a9312bd5d0>
2015-02-09 09:38:52 +0000

    adding an autocomplete option for drm4g command

--- drm4g_init.sh
+++ drm4g_init.sh
@@ -1,2 +1,3 @@
-export PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ):$PATH
-source ./drm4g_autocomplete.sh
+DRM4G_BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export PATH=${DRM4G_BIN}:$PATH
+source ${DRM4G_BIN}/drm4g_autocomplete.sh
