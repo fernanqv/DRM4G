87c2c42e00ffd32663e9dd88bfebc91dc86f6242
blancojc <josecarlosblancoreal@gmail.com>
2016-08-11 17:33:58 +0200

    adding readme file

--- bin/drm4g_init.sh
+++
@@ -1,5 +0,0 @@
-DRM4G_DEPLOYMENT_BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DRM4G_DEPLOYMENT_DIR=$( dirname $DRM4G_DEPLOYMENT_BIN )
-if [[ ":$PATH:" != *":$DRM4G_DEPLOYMENT_BIN:"* ]]; then export PATH=$DRM4G_DEPLOYMENT_BIN:${PATH}; fi
-if [[ ":$PYTHONPATH:" != *":$DRM4G_DEPLOYMENT_DIR/libexec:"* ]]; then export PYTHONPATH=$DRM4G_DEPLOYMENT_DIR/libexec:${PYTHONPATH}; fi
-source ${DRM4G_DEPLOYMENT_BIN}/drm4g_autocomplete.sh
