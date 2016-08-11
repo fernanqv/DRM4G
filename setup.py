e0ad0dc46aa3b9f2c822e5d27594c106fd873189
Antonio Minondo <antonio.minondo@alumnos.unican.com>
2016-08-11 15:09:56 +0200

    Made changes Carlos asked me to do before doing a push to the remote repository.

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ import subprocess
 import glob
 import sys
 import ast
+import shutil #this package might not work on Mac
 #from distutils.core import setup
 #from distutils.command.build import build
 
 #To ensure a script runs with a minimal version requirement of the Python interpreter
 #assert sys.version_info >= (2,5)
+
 '''
 >>> from platform import python_version
 >>> python_version()
@@ -24,16 +26,15 @@ sys.version_info(major=2, minor=7, micro=12, releaselevel='final', serial=0)
 '2.7.12 (default, Jul  1 2016, 15:12:24) \n[GCC 5.4.0 20160609]'
 '''
 
+here = path.abspath(path.dirname(__file__))
 python_ver=sys.version[:3]
 user_shell=os.environ['SHELL']
 
-#I make bash a special case because of what is said here http://superuser.com/questions/49289/what-is-the-bashrc-file
+#I consider bash a special case because of what is said here http://superuser.com/questions/49289/what-is-the-bashrc-file
 if 'bash' in user_shell:
     user_shell='.bashrc'
 else:
     user_shell='.profile'
-
-here = path.abspath(path.dirname(__file__))
 
 
 def get_long_description():
@@ -51,9 +52,9 @@ def get_long_description():
         description = open(readme_file).read()
     return description
 
-
 class Builder(object):
 
+    export_dir=''
     prefix_directory=''
     arguments=str(sys.argv)
     arguments=ast.literal_eval(arguments) #convert from string to list
@@ -71,39 +72,28 @@ class Builder(object):
             option=self.arguments[i]
             #folder name can't contain '--prefix' or '--home'
             if '--prefix' in option or '--home' in option:
-                export_dir=''
                 lib_dir=''
-                #I'm working under the impression that the path passed on to prefix has to be an absolute path CHECK IT
+                #I'm working under the impression that the path passed on to prefix has to be an absolute path - for the moment, if you use a relative path, gridway's binary files will be copied to a directory relative to where ./gridway-5.8 is
                 if '=' in option:
                     #this is assuming that spaces are not permited (--prefix=/home) TESTED
                     #self.prefix_directory=option #now that I added 'home' in the if I can't have it like this, since configure doesn't recognize --home as an option
-                    export_dir=option[option.find('=')+1:] #substring that goes from the position after the '=' till the end 
-                    self.prefix_directory='--prefix '+export_dir
+                    self.export_dir=option[option.find('=')+1:] #substring that goes from the position after the '=' till the end 
+                    self.prefix_directory='--prefix '+self.export_dir
                 else:
                     #self.prefix_directory=option+' '+self.arguments[i+1]
-                    export_dir=self.arguments[i+1]
-                    self.prefix_directory='--prefix '+export_dir
+                    self.export_dir=self.arguments[i+1]
+                    self.prefix_directory='--prefix '+self.export_dir
 
                 if '--prefix' in option:
-                    #maybe it would be better to just use export_dir instead of lib_dir
-                    lib_dir=os.path.join(export_dir,'lib/python{}/site-packages'.format(python_ver))
+                    lib_dir=os.path.join(self.export_dir,'lib/python{}/site-packages'.format(python_ver))
                 elif '--home' in option:
-                    lib_dir=os.path.join(export_dir,'lib/python')
+                    lib_dir=os.path.join(self.export_dir,'lib/python')
 
                 try:
                     os.makedirs(lib_dir)
                 except OSError:
-                    print('\nDirectory already exists\n')
-                '''
-                Creo que es este el que me ha soltado este error
-                antonio@diluvio:~/Documentos/drm4g_source/build_3/source_code$ python setup.py install --prefix=/home/antonio/Documentos/drm4g_source/build_3/drm4g
-                running install
-                #####################
-                AN ERROR OCCURED WHILE DOING THE BUILD
-                #####################
-                [Errno 17] File exists: '/home/antonio/Documentos/drm4g_source/build_3/drm4g/lib/python2.7/site-packages'
-                error: [Errno 17] File exists: '/home/antonio/Documentos/drm4g_source/build_3/drm4g/lib/python2.7/site-packages'
-                '''
+                    print('\nDirectory {} already exists'.format(lib_dir))
+
                 #ahora seria crear un fichero drm4g.pth en site-packages/drm4g, el fichero tendra una sloa linea 'drm4g'
                 #depues llamar al metodo site.addsitedir para que lo ejecute
                 '''
@@ -112,59 +102,42 @@ class Builder(object):
                     Add a directory to sys.path and process its .pth files. Typically used in sitecustomize or usercustomize (see above).
                 '''
 
-                '''
-                python_path=os.environ.get('PYTHONPATH')
-                if python_path:
-                    python_path=python_path.split(':') #os.environ.get returns a string of paths separated by ':', here it's being converted to a list
-                    if not lib_dir in python_path:
-                        #if lib_dir happens to be a subpath(substring) from a path(string) in the variable, this will still give true (which is bad)
-
-
-                if not export_dir in sys.path:
-                    #sys.path.append('lib_dir') #didn't work
-                    self.call('echo "export PYTHONPATH={}:$PYTHONPATH" >> ~/{}'.format(export_dir,user_shell))
-                    #self.call('source ~/.bashrc') #need to reload .bashrc so that PYTHONPATH gets defined
-                
-                #as I said before, it didn't work
-                print '\n\n\n#############\n'+','.join(sys.path)+'\n#############\n\n'+lib_dir+'\n'+sys.executable
-                #sys.path.append(lib_dir)
-                sys.path.insert(1,lib_dir)
-                print '\n\n\n#############\n'+','.join(sys.path)+'\n#############\n\n'+lib_dir+'\n'+sys.executable
-                '''
-                #RAW_INPUT - 2.7 ---- INPUT - 3.5
-                ans=''
-                print("\nWe are about to modify your {} file.\n" \
+                message="\nWe are about to modify your {} file.\n" \
                     "If we don't you'll have to define PYTHONPATH" \
                     " or manually find and execute DRM4G everytime you wish to use it.\n" \
-                    "Do you want us to continue?\ny -- yes\nn -- no".format(user_shell))
-                if python_ver[0] == '2':
-                    while((ans!='y') and (ans!='Y') and (ans!='yes') and (ans!='Yes') and (ans!='n') and (ans!='N') and (ans!='no') and (ans!='No')):
-                        ans=raw_input()
-                else:
-                    while((ans!='y') and (ans!='Y') and (ans!='yes') and (ans!='Yes') and (ans!='n') and (ans!='N') and (ans!='no') and (ans!='No')):
-                        ans=input()
-                
-                if((ans=='y') or (ans=='Y') or (ans=='yes') or (ans=='Yes')):
+                    "Do you want us to continue?".format(user_shell)
+
+                ans=self.yes_no_choice(message)
+                if ans[0]=='y':
                     #when installing they'll have to define the variable PYTHONPATH, but with this, they won't have to do it again in order to use DRM4G
-                    self.call('echo "export PYTHONPATH={}:$PYTHONPATH" >> ~/{}'.format(export_dir,user_shell))
+                    #self.call('echo "export PYTHONPATH={}:$PYTHONPATH" >> ~/{}'.format(self.export_dir,user_shell))
+                    home=os.path.expanduser('~') #to ensure that it will find $HOME directory in all platforms
+                    with open('{}/{}'.format(home,user_shell),'a') as f:
+                        f.write('export PYTHONPATH={}:$PYTHONPATH'.format(self.export_dir))
+    """
+    Ask for Yes/No questions
+    """
+    def yes_no_choice(self, message,  default = 'y') :
+        PY2 = python_ver[0] == '2'
+        choices = 'Y/n' if default.lower() in ( 'y', 'yes' ) else 'y/N'
+        values = ( 'y', 'yes', 'n', 'no' )
+        choice = ''
+        if PY2:
+            while not choice.strip().lower() in values:
+                choice = raw_input( "%s \n(%s) " % ( message, choices ) )
+        else:
+            while not choice.strip().lower() in values:
+                choice = input( "%s \n(%s) " % ( message, choices ) )
+        
+        return choice.strip().lower()
 
-            #if '--home' in option:
-                
-        #print '\n\n\n\n{}\n\n\n\n'.format(prefix_directory)
-
-
+    
     [] #I don't know what's this doing here (it was uncommented)
     def build(self):
-        # self.call('sudo rm %s' % filename) --- filename = '/usr/local/lib/libhdf5.so.8.0.1
-        #cd gridway-5.8; ./configure --prefix=$HOME/Documentos/drm4g_source/build_3/drm4g; make; make install; cd ..; cp ./bin/* $HOME/Documentos/drm4g_source/build_3/drm4g/bin; 
-        #export PYTHONPATH=%s:$PYTHONPATH && -- lo anadi antes porque me d
-        #self.call('export PYTHONPATH=%s:$PYTHONPATH' % drm4g_install_dir) #se necesita antes - That .pth error is a build check that python-setuptools has.
-        #self.call('echo $PYTHONPATH')
-        #self.call(('cd gridway-5.8 && ./configure %s && make && make install && make clear') % (config['post']))
         gridway=path.join( here, "gridway-5.8")
         current_path = os.getcwd()
-        try:
 
+        try:
             self.prefix_option()
             
             if not path.exists(gridway) :
@@ -172,21 +145,48 @@ class Builder(object):
 
             os.chdir( gridway )
 
-
-            #self.call('pwd')
-            #print '\n\n\n\n{}\n\n\n\n'.format(self.prefix_directory)
-            exit_code = self.call('./configure %s' % self.prefix_directory)
+            exit_code = self.call('./configure --prefix={}'.format(path.join(here,'build/temp.gridway-5.8')))
             if exit_code:
                 raise Exception("Configure failed - check config.log for more detailed information")
             
             exit_code = self.call('make')
             if exit_code:
                 raise Exception("make failed")
-            
+
+            drm4g_bin=path.join(self.export_dir,'bin')
+            if not path.exists(drm4g_bin) : 
+                #raise Exception("The specified directory %s doesn't exist" % path.join(self.export_dir,'bin'))
+                try:
+                    os.makedirs(drm4g_bin)
+                except OSError:
+                    print('\nDirectory {} already exists'.format(drm4g_bin))
+
+            files=glob.glob('src/cmds/gw[!_]*') #files=glob.glob('src/cmds/gw[^_]*')
+            print files
+            for f in files:
+                shutil.copy(f,drm4g_bin)
+            files=['src/gwd/gwd','src/scheduler/gw_flood_scheduler','src/scheduler/gw_sched']
+            for f in files:
+                #shutil.copy(path.join(gridway,f),path.join(self.export_dir,'bin'))
+                shutil.copy(f,path.join(self.export_dir,'bin'))
+
+            '''
+            this function works but it copies a few unnecessary files as well
+            dirs=['src/cmds','src/gwd','src/scheduler']
+            def copy_binary(dirs):
+                 for d in dirs:
+                     files=os.listdir(d)
+                     for f in files:
+                         if os.path.isfile(os.path.join(d,f)):
+                             if mimetypes.guess_type(os.path.join(d,f))[0] == None:
+                                 shutil.copy(os.path.join(d,f),os.path.join(self.export_dir,'bin'))
+            '''
+
+            '''
             exit_code = self.call('make install')
             if exit_code:
                 raise Exception("make install failed")
-                        
+            '''            
             exit_code = self.call('make clean')
             if exit_code:
                 print("make clean failed")
@@ -198,33 +198,13 @@ class Builder(object):
         finally:
             os.chdir( current_path )
 
-
-        
         #self.call('echo "#####################\nSKIPPING CONFIGURE && MAKE\n#####################"')
 
-
-'''class build_wrapper(build):
-    def initialize_options(self):
-        # Deploy all the described libraries in the BINARIES dictionary.
-        #libs='drm4g'
-        #build_lib = lambda lib: Builder(lib).build()
-        #build_lib = lambda lib: Builder().build()
-        #map(build_lib, libs)
-        #build_lib(libs)
-        Builder().build()
-        return build.initialize_options(self)
-'''
 class build_wrapper(DistutilsInstall):
     def run(self):
         Builder().build()
         DistutilsInstall.run(self)
 
-#version='2.0', # or the method found in netcdf's implementation could work 'calculate_version'
-#packages=['drm4g'], #probably shouldn't add this (I had gridway-5.8 in there, but this is only to address pure Python modules, not C)
-#py_modules=,['bin.drm4g.py'], #no since there is no __init__.py in the folder
-#hacia falta instalar/incluir un header file?
-#requires='paramiko<2.0',
-#2.7 Installing Additional Files, me incumbe?
 bin_scripts= glob.glob(os.path.join('bin', '*'))
 #conf_files= glob.glob(os.path.join('etc', '*'))
 
@@ -259,3 +239,4 @@ setup(
         'install': build_wrapper,
     },
 )
+
