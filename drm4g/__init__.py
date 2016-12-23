17ad98d96b482725859f5d96327e67d8ff7280f5
Antonio Minondo <antonio.minondo@unican.es>
2016-12-23 18:19:29 +0100

    Changed the DRM4G package version to 2.6.1

--- drm4g/__init__.py
+++ drm4g/__init__.py
@@ -20,7 +20,7 @@
 
 __all__ = ["communicators", "core", "managers", "utils", "commands", "api"]
 
-__version__  = '2.6.0'
+__version__  = '2.6.1'
 __author__   = 'Carlos Blanco and Antonio Minondo'
 __revision__ = "$Id$"
 
